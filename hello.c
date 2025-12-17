21167d25a81432a00e4779e3e8f4c93491c76478
zengxunxun <zengxunxun@gmail.com>
2025-12-17 11:40:06 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*
+ * a.c
+ * Copyright (C) 2025 zengxunxun <zengxunxun@debian-1>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "stdio.h"
+
+int main() {
+    printf("hello world\n");
+}
